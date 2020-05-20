f695b3f538a7ef666cd0c9956210864be4dc2ca1
Renan Zulian <renanzulian@gmail.com>
2020-05-19 22:36:38 -0300

    Criando o componente das empresas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EnterpriseComponent } from './pages/enterprise/enterprise.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EnterpriseComponent }
+];
 
 @NgModule({
+  declarations: [EnterpriseComponent],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
